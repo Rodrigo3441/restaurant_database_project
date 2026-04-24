6f6db951f9bbcb46757a805e084ea026b2726f91
Rodrigo <119820531+Rodrigo3441@users.noreply.github.com>
2026-04-23 22:20:20 -0300

    database: updated databaseconnector to postgresql

--- java/ProjetoRestaurante/src/database/DatabaseConnection.java
+++ java/ProjetoRestaurante/src/database/DatabaseConnection.java
@@ -20,8 +20,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-	private static final String URL = "jdbc:mysql://localhost:3306/Fred_Food";
-	private static final String USER = "root";
+	private static final String URL = "jdbc:postgresql://localhost:5432/Fred_Food";
+	private static final String USER = "postgres";
 	private static final String PASSWORD = "root";
 	
 	/**
