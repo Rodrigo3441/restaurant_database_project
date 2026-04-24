a45afb60d7f99525f99588106791e30434b9695f
Rodrigo <119820531+Rodrigo3441@users.noreply.github.com>
2026-04-23 22:23:10 -0300

    sql: updated some project files

--- java/ProjetoRestaurante/src/application/Programa.java
+++ java/ProjetoRestaurante/src/application/Programa.java
@@ -3,48 +3,21 @@ package application;
 import java.sql.Connection;
 import database.DatabaseConnection;
 
-import entities.Produto;
-import entities.Restaurante;
-import database.AcessoDadosProduto;
-import database.AcessoDadosRestaurante;
+import ui.MenuPrincipal;
+import ui.MenuRestaurante;
 
-import java.util.ArrayList;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-		//definição da conexão
-		System.out.println("TEste");
+	
+
 		Connection conn = DatabaseConnection.getConnection();
 		
+		MenuPrincipal mn = new MenuPrincipal(conn);
+		mn.mostrar();
 		
-		System.out.println("Connected successfully!");
-
-		AcessoDadosRestaurante restconn = new AcessoDadosRestaurante(conn);
-		
-
-		//insere um novo restaurante
-		Restaurante alvo = new Restaurante("1234", "Batata Top", "119857383", 1, "senha123");
-				
-		//executa e verifica se foi inserido corretamente
-		if (restconn.inserirRestaurante(alvo)) {
-			System.out.println("Restaurante cadastrado com sucesso!");
-		};
-
-		//imprime o nome atual
-		System.out.println(restconn.retornarRestaurante("1234").getNome());
-
-		//atualiza o nome com um novo
-		alvo.setNome("Batata Não Top");
-
-		//envia o objeto com as novas informações para a camada de manipulação
-		if (restconn.atualizarRestaurante(alvo)) {
-			System.out.println("Restaurante alterado com sucesso!");
-		};
-			
-		//imprime o novo nome
-		System.out.println(restconn.retornarRestaurante("1234").getNome());
 	}
 }
