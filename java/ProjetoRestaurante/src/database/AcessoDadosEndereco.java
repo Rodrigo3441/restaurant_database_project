51792f80d1e915ad503556a040a8c40c216553e6
Rodrigo <119820531+Rodrigo3441@users.noreply.github.com>
2026-04-25 01:13:31 -0300

    database: created AddressDAO class to connect to the database

---
+++ java/ProjetoRestaurante/src/database/AcessoDadosEndereco.java
@@ -0,0 +1,149 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import entities.EnderecoRestaurante;
+
+/**
+ * Classe: AcessoDadosEndereco
+ *
+ * Descrição:
+ * Classe responsável por gerenciar dados do Endereco
+ *
+ * Responsabilidades:
+ * - Conectar ao banco de dados
+ * - Fazer manipulações com os dados
+ *
+ * @author Rodrigo
+ * @since 24-04-2026
+ */
+
+public class AcessoDadosEndereco {
+	
+	private Connection conn;
+	
+	/**
+	 * Construtor
+	 * Recebe a conexão para que seja possível estabelecer
+	 * uma comunicação com o banco de dados
+	 * 
+	 * @param conn: objeto de conexão
+	 */
+	public AcessoDadosEndereco(Connection conn) {
+		this.conn = conn;
+	}
+	
+	/**
+	 * Método inserirEnderecoRestaurante
+	 * 
+	 * Responsável for receber um objeto de endereço do restaurante e inserir no banco de dados
+	 * 
+	 * @param er objeto do tipo EnderecoRestaurante
+	 * @return true ou false
+	 */
+	public boolean inserirEnderecoRestaurante(EnderecoRestaurante er) {
+		String sqlQuery = "INSERT INTO ENDERECO_RESTAURANTE "
+				+ "(pk_fk_res_cnpj, "
+				+ "pk_enr_cep, "
+				+ "enr_nome, "
+				+ "enr_numero) VALUES (?, ?, ?, ?)";
+		
+		//preparação da query antes da execução
+		try (PreparedStatement stmt = conn.prepareStatement(sqlQuery)){
+			
+			//vinculação dos atributos à query preparada
+			stmt.setString(1, er.getCnpjRestaurante());
+			stmt.setString(2, er.getCep());
+			stmt.setString(3, er.getNome());
+			stmt.setInt(4, er.getNumero());
+			
+			int linhasAfetadas = stmt.executeUpdate();
+			return linhasAfetadas > 0;
+			
+		} catch (SQLException e) {
+			System.err.println("Erro na operação de ENDERECO_RESTAURANTE");
+		    e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Método retornarEnderecoRestaurante
+	 * 
+	 * Responsável por trazer as informações do endereço do restaurante
+	 * para que possam ser utilizadas ao longo da sessão
+	 *  
+	 * @param cnpj do restaurante
+	 * @return objeto do tipo Endereco ou false
+	 */
+	public EnderecoRestaurante retornarEnderecoRestaurante(String cnpj) {
+		String sqlQuery = "SELECT * FROM ENDERECO_RESTAURANTE WHERE pk_fk_res_cnpj = ?";
+		
+		//preparação da query antes da execução
+		try (PreparedStatement stmt = conn.prepareStatement(sqlQuery)){
+			
+			//vinculação dos atributos à query preparada
+			stmt.setString(1, cnpj);
+			
+			ResultSet resultado = stmt.executeQuery();
+			
+			//se houver resultado da busca pelo cpnj, instancia um objeto restaurante
+			//com os atributos do resultado
+			if (resultado.next()) {
+				EnderecoRestaurante er = new EnderecoRestaurante();
+				
+				er.setCnpjRestaurante(resultado.getString("pk_fk_res_cnpj"));
+				er.setCep(resultado.getString("pk_enr_cep"));
+				er.setNome(resultado.getString("enr_nome"));
+				er.setNumero(resultado.getInt("enr_numero"));
+				
+				return er;
+
+			}
+									
+		} catch (SQLException e) {
+			System.err.println("Erro na operação de ENDERECO_RESTAURANTE");
+		    e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * Método atualizarEnderecoRestaurante
+	 * 
+	 * Responsável por atualizar as informações do endereço de um restaurante no banco de dados 
+	 * 
+	 * @param er: objeto EnderecoRestaurante
+	 */
+	public boolean atualizarEnderecoRestaurante(EnderecoRestaurante er) {
+		String sqlQuery = "UPDATE ENDERECO_RESTAURANTE SET " +
+	            "pk_enr_cep = ?, " +
+	            "enr_nome = ?, " +
+	            "enr_numero = ? " +
+	            "WHERE pk_fk_res_cnpj = ?";
+
+		//preparação da query antes da execução
+		try (PreparedStatement stmt = conn.prepareStatement(sqlQuery)){
+			
+			//vinculação dos atributos à query preparada
+			stmt.setString(1, er.getCep());
+	        stmt.setString(2, er.getNome());
+	        stmt.setInt(3, er.getNumero());
+	        stmt.setString(4, er.getCnpjRestaurante());
+						
+			int linhasAfetadas = stmt.executeUpdate();
+			return linhasAfetadas > 0;
+			
+		} catch (SQLException e) {
+			System.err.println("Erro na operação de ENDERECO_RESTAURANTE");
+		    e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+}
