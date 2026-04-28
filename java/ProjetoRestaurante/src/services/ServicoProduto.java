6ed19bda4cc78c0200b41241a2d3a22d86cee911
Rodrigo <119820531+Rodrigo3441@users.noreply.github.com>
2026-04-27 21:51:09 -0300

    ServicoProduto: created ServicoProduto class, that will handle all rules related to Products

---
+++ java/ProjetoRestaurante/src/services/ServicoProduto.java
@@ -0,0 +1,112 @@
+package services;
+
+import database.AcessoDadosProduto;
+import database.DatabaseConnection;
+import entities.Produto;
+
+/**
+ * Classe: ServicoProduto
+ *
+ * Descrição:
+ * Classe responsável por gerenciar as regras de negócio do produto
+ *
+ * Responsabilidades:
+ * - oferecer métodos de validação das informações
+ * - se comunicar com a camada de dados
+ *
+ * @author Rodrigo
+ * @since 27-04-2026
+ */
+public class ServicoProduto {
+	//conexão com o banco de dados que será usada em todas as operações
+	private AcessoDadosProduto dao = new AcessoDadosProduto(DatabaseConnection.getConnection());
+	
+	public ServicoProduto() {
+		
+	}
+
+	/**
+	 * 
+	 * @param nome
+	 */
+	public void validarNome(String nome) {
+		if (!nomeValido(nome)) {
+			throw new IllegalArgumentException("Digite um nome válido");
+		}
+	}
+	
+	/**
+	 * 
+	 * @param descricao
+	 */
+	public void validarDescricao(String descricao) {
+		if (!descricaoValida(descricao)) {
+			throw new IllegalArgumentException("Digite uma descrição válida");
+		}
+	}
+	
+	/**
+	 * 
+	 * @param codigo
+	 */
+	public void validarCodigo(int codigo) {
+		if (!codigoValido(codigo)) {
+			throw new IllegalArgumentException("Digite um código válido");
+		}
+		
+		if (!codigoDisponivel(codigo)) {
+			throw new IllegalArgumentException("Esse código já está em uso. Tente outro");
+		}
+	}
+	
+	/**
+	 * 
+	 * @param nome
+	 * @return
+	 */
+	public Produto buscarProdutoPorNome(String nome) {
+		nome = nome.toLowerCase().trim();
+		return dao.retornarProdutoPorNome(nome);
+	}
+	
+	public boolean inserirProduto(Produto p) {
+		return dao.inserirProduto(p);
+	}
+	
+	/**
+	 * 
+	 * @param nome
+	 * @return
+	 */
+	private boolean nomeValido(String nome) {
+		return nome.length() >= 3 && nome.length() < 40;
+	}
+	
+	/**
+	 * 
+	 * @param descricao
+	 * @return
+	 */
+	private boolean descricaoValida(String descricao) {
+		return descricao.length() < 255;
+	}
+	
+	/**
+	 * 
+	 * @param codigo
+	 * @return
+	 */
+	private boolean codigoValido(int codigo) {
+		return codigo > 0 && codigo < 2_000_000_000;
+	}
+	
+	/**
+	 * 
+	 * @param codigo
+	 * @return
+	 */
+	private boolean codigoDisponivel(int codigo) {
+		return dao.retornarProdutoPorId(codigo) == null;
+	}
+	
+}
