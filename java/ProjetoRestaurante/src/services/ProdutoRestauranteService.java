5157af976a707d804bd200a9c64cf984efd7ff72
Rodrigo <119820531+Rodrigo3441@users.noreply.github.com>
2026-04-28 21:30:16 -0300

    services: created service validations for ProdutoRestaurante

---
+++ java/ProjetoRestaurante/src/services/ProdutoRestauranteService.java
@@ -0,0 +1,115 @@
+package services;
+
+import java.util.ArrayList;
+
+import database.ProdutoRestauranteDAO;
+import database.DatabaseConnection;
+import entities.ProdutoRestaurante;
+import view.ProdutoRestauranteView;
+
+public class ProdutoRestauranteService {
+
+	//conexão com o banco de dados que será usada em todas as operações
+	private ProdutoRestauranteDAO dao = new ProdutoRestauranteDAO(DatabaseConnection.getConnection());
+	
+	/**
+	 * 
+	 * @param quantidadeEstoque
+	 */
+	public void validarQuantidadeEstoque(int quantidadeEstoque) {
+		if(!quantidadeEstoqueValida(quantidadeEstoque)) {
+			throw new IllegalArgumentException("Insira uma quantidade de estoque válida");
+		}
+	}
+	
+	/**
+	 * 
+	 * @param preco
+	 */
+	public void validarPrecoProduto(double preco) {
+		if(!precoProdutoValido(preco)) {
+			throw new IllegalArgumentException("Digite um preço válido para o produto");
+		}
+	}
+	
+	/**
+	 * 
+	 * @param codigo
+	 */
+	public void validarCodigoProduto(int codigo) {
+		if(!codigoProdutoValido(codigo)) {
+			throw new IllegalArgumentException("Digite um código válido");
+		}
+	}
+	
+	/**
+	 * 
+	 * @param cnpj
+	 * @param codigo
+	 * @return
+	 */
+	public boolean produtoJaEstaCadastrado(String cnpj, int codigo) {
+		return dao.produtoJaEstaCadastrado(cnpj, codigo);
+	}
+	
+	/**
+	 * 
+	 * @param cnpj
+	 * @param codigo
+	 * @throws Exception
+	 */
+	public void apagarProdutoRestaurante(String cnpj, int codigo) throws Exception {
+		if(!dao.deletarProduto(cnpj, codigo)) {
+			throw new Exception("Ocorreu um erro desconhecido ao apagar o produto.");		
+		} 
+		
+	}
+	
+	/**
+	 * 
+	 * @param pr
+	 * @return
+	 */
+	public boolean associarProdutoRestaurante(ProdutoRestaurante pr) {
+		return dao.associarProdutoRestaurante(pr);
+	}
+	
+	/**
+	 * Recebe o cnpj do restaurante em sessão e retorna todos os 
+	 * produtos cadastrados para aquele restaurante
+	 * @param cnpj do restaurante em sessão
+	 * @return arraylista com todos os produtos do restaurante
+	 */
+	public ArrayList<ProdutoRestauranteView> retornarTodoProdutoRestaurante(String cnpj){
+		return dao.retornarTodoProdutoRestaurante(cnpj);
+	}
+	
+	/**
+	 * 
+	 * @param quantidadeEstoque
+	 * @return
+	 */
+	private boolean quantidadeEstoqueValida(int quantidadeEstoque) {
+		return quantidadeEstoque >= 0;
+	}
+	
+	/**
+	 * 
+	 * @param preco
+	 * @return
+	 */
+	private boolean precoProdutoValido(double preco) {
+		return preco > 0;
+	}
+	
+	/**
+	 * 
+	 * @param codigo
+	 * @return
+	 */
+	private boolean codigoProdutoValido(int codigo) {
+		return codigo > 0 && codigo < 2_000_000_000;
+	}
+	
+	
+}
