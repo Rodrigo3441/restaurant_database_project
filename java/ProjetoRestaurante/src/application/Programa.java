90003e5a91d152d6aced6d28054fed720a5ae94c
Rodrigo <119820531+Rodrigo3441@users.noreply.github.com>
2026-04-30 22:43:43 -0300

    main: some small changes for testing purposes

--- java/ProjetoRestaurante/src/application/Programa.java
+++ java/ProjetoRestaurante/src/application/Programa.java
@@ -2,18 +2,13 @@ package application;
 
 import java.sql.Connection;
 import database.DatabaseConnection;
-
 import ui.MenuPrincipal;
-import ui.MenuProdutoRestaurante;
 
 
 
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	
-	
 
 		Connection conn = DatabaseConnection.getConnection();
 		
